4946d66aa14505efdad51c3625ef5014ff3b9f91
Paul Nathan <hello@paulnathan.io>
2026-04-08 09:39:06 -0400

    Fix ArgoCD apps

---
+++ onboarding.sh
@@ -0,0 +1,47 @@
+# !bin/bash
+
+omnictl kubeconfig --cluster talos-default --service-account --user talos-prod-sa --force
+
+export OP_CREDENTIALS=$(op read op://k8s/1passwordconnect/1password-credentials.json)
+export OP_CONNECT_TOKEN=$(op read op://k8s/1passwordconnect/connect-token)
+
+cilium install \
+    --version 1.19.2 \
+    --set cluster.name=talos-default \
+    --set ipam.mode=kubernetes \
+    --set kubeProxyReplacement=true \
+    --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
+    --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+    --set cgroup.autoMount.enabled=false \
+    --set cgroup.hostRoot=/sys/fs/cgroup \
+    --set k8sServiceHost=localhost \
+    --set k8sServicePort=7445 \
+    --set hubble.enabled=false \
+    --set hubble.relay.enabled=false \
+    --set hubble.ui.enabled=false \
+    --set gatewayAPI.enabled=true \
+    --set gatewayAPI.enableAlpn=true \
+    --set gatewayAPI.enableAppProtocol=true
+
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/standard-install.yaml
+kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/experimental-install.yaml
+
+kubectl create namespace 1passwordconnect
+kubectl create namespace external-secrets
+
+kubectl create secret generic 1password-credentials \
+  --namespace 1passwordconnect \
+  --from-literal=1password-credentials.json="$OP_CREDENTIALS"
+
+kubectl create secret generic 1password-operator-token \
+  --namespace 1passwordconnect \
+  --from-literal=token="$OP_CONNECT_TOKEN"
+
+kubectl create secret generic 1passwordconnect \
+  --namespace external-secrets \
+  --from-literal=token="$OP_CONNECT_TOKEN"
+
+echo "sleeping for helm"
+sleep 30
+
+./scripts/bootstrap-argocd.sh
